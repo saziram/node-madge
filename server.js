30077bd6b5f4a4708a7e4de509f387f848f479dd
saziram <48680927+saziram@users.noreply.github.com>
2021-04-15 19:29:54 +0530

    code changes

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-const os = require('os');
-const path = require('path');
-const fs = require('mz/fs');
 const madge = require('./lib/api');
-
 const express = require("express"), app = express();
-const dotENV = require("dotenv");
-const bodyParser = require("body-parser");
-const { headers } = require('./server/middlewares/headers');
-//const dbConfig = require("./server/config/dbConfig.json");
 const router = require('./server/middlewares/router');
 
-// middlewares
-app.use(bodyParser.json());
-app.use(headers);
 app.use(router);
 
-app.listen(process.env.PORT || 4000, () => {
-	dotENV.config({ path: "./server/.env" });    
+app.listen(process.env.PORT || 4000, () => {   
 	console.log('Server listening at 4000');
 });  
